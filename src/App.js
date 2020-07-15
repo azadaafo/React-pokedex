ea024faff44f53f1efc4f88155defb707dc0211d
azadaafo <azadaafo@gmail.com>
2020-07-15 18:55:31 +0100

    added Logo and BestPokemon functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function Logo() {
+  return (
+    <header>
+      <h1>Welcome to the Pokedex</h1>
+      <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt='' />
+    </header>
+  )
+}
+
+function BestPokemon() {
+  return (
+    <p>My favourite Pokemon is Squirtle</p>
+  )
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>Welcome to the Pokedex</h1>
-        <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt='' />
-      </header>
+      <Logo />
+      <BestPokemon />
+
 
     </div>
 
