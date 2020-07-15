060d96ce70ee6b731012fe78dd8de35d9b8fe888
azadaafo <azadaafo@gmail.com>
2020-07-15 23:51:11 +0100

    added new component, list, date, and Pokemon abilities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react'
 
+function CaughtPokemon() {
+  const date = new Date().toLocaleDateString()
+  return <p>Caught 0 Pokemon on {date}</p>
+}
+
 const Logo = () => {
+  const appName = "Pokedex";
   return (
     <header>
-      <h1>Welcome to the Pokedex</h1>
+      <h1>Welcome to the {appName}</h1>
       <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt='' />
     </header>
   )
 }
 
-const BestPokemon = () => (
-  <p>My favourite Pokemon is Squirtle</p>
-)
+function BestPokemon() {
+  const abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
+  return (
+    <div>
+      <p>My favourite Pokemon is Squirtle</p>
+      <ul>
+        {abilities.map(ability => <li>{ability}</li>)}
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <Logo />
       <BestPokemon />
-
-
+      <CaughtPokemon />
     </div>
 
   )
