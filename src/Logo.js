0b75fb2447668d298a5a4f75258f44f8e70cfc2d
azadaafo <azadaafo@gmail.com>
2020-07-19 15:18:17 +0100

    Exercise A done: added a function, logged a message, added onclick handler to the img

--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,17 @@
 import React from 'react'
 const Logo = (props) => {
+
+    function logWhenClicked() {
+        console.log("it doesn't matter what the message is");
+    }
+
     console.log(props);
     return (
         <header>
             <h1>Welcome to the {props.appName}</h1>
             <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt='' />
+            <button onClick={logWhenClicked}>Click me!</button>
+
         </header>
     )
 }
