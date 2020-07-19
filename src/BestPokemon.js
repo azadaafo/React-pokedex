0c2bc14dc2fa44297da7fcd3a11a43109791501e
azadaafo <azadaafo@gmail.com>
2020-07-20 00:39:06 +0100

    added BestPokemonFetcher component and useEffect hook per exercise E

--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function BestPokemon(props) {
-    console.log(props);
+const BestPokemonFetcher = () => {
+    const [bestPokemon, setBestPokemon] = useState(null);
+
+    useEffect(() => {
+        fetch(
+            'https://pokeapi.co/api/v2/pokemon/1/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setBestPokemon(data)
+            })
+    }, []);
+
+    if (!bestPokemon) {
+        return null
+    } else {
+        return <BestPokemon pokemon={bestPokemon} />;
+    }
+}
+
+const BestPokemon = ({ pokemon }) => {
     return (
         <div>
-            <p>My favourite Pokemon is Squirtle</p>
+            <p>My favourite Pokemon is {pokemon.name}</p>
             <ul>
-                {props.abilities.map(ability => <li>{ability}</li>)}
+                {pokemon.abilities.map((ability, index) => {
+                    return <li key={index}>{ability.ability.name}</li>
+                })}
             </ul>
         </div>
     )
 }
 
-export default BestPokemon;
+export default BestPokemonFetcher;
