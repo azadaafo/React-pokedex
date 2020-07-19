6fd17544afb80878842409986535ad5671c951b5
azadaafo <azadaafo@gmail.com>
2020-07-19 15:51:31 +0100

    exercise B done: passed the logWhenClicked function reference as a prop to the Logo component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import CaughtPokemon from './CaughtPokemon';
 import BestPokemon from './BestPokemon';
 
 function App(props) {
+  function logWhenClicked() {
+    console.log("it doesn't matter what the message is");
+  }
   return (
     <div>
-      <Logo appName="Pokedex" />
+      <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={['Anticipation', 'Adaptability', 'Run-Away']} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
     </div>
--- src/Logo.js
+++ src/Logo.js
@@ -10,7 +10,7 @@ const Logo = (props) => {
         <header>
             <h1>Welcome to the {props.appName}</h1>
             <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png' alt='' />
-            <button onClick={logWhenClicked}>Click me!</button>
+            <button onClick={props.handleClick} >Click me!</button>
 
         </header>
     )
